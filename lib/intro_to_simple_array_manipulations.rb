4ecf3432ae57fe6efd1abed932705408b0b049a7
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-24 19:29:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,18 @@
 def using_push(array, string) 
   array.push(string)
-  
-  
 end 
+
+
+def using_unshift(array, sting)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+
+
+
+
+
